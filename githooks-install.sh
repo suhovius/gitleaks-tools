440802843f83d81d52524be053c41000ff649f24
Suhovius <alexey.suhoviy@gmail.com>
2024-08-26 17:56:39 +0300

    Refactor githooks install

--- githooks-install.sh
+++ githooks-install.sh
@@ -3,17 +3,18 @@
 checkIfPreCommitExists() {
 	if [ -f .git/hooks/pre-commit ]; then
 		echo "WARNING! .git/hooks/pre-commit file alerady exists!"
-		read -p "Override?? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
-
+		read -p "Override? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+		echo "Overriding the existing .git/hooks/pre-commit file"
 		downloadAndInstallPreCommitGitHook
 	else
+		echo "Installing new .git/hooks/pre-commit file"
 		downloadAndInstallPreCommitGitHook
 	fi
 }
 
 ensureGitleaksInstalled() {
   # Check if gitleaks is installed
-  if ! type "gitleaks" > /dev/null; then
+  if ! [ -x "$(command -v gitleaks)" ]; then
     echo "gitleaks not found. Installing gitleaks..."
     curl -s https://raw.githubusercontent.com/suhovius/gitleaks-tools/main/install.sh | bash
 
@@ -21,13 +22,17 @@ ensureGitleaksInstalled() {
       echo "Failed to install gitleaks. Aborting commit."
       exit 1
     fi
+  else
+    echo "gitleaks found. Skip gitleaks command install. Proceed with git hooks installation."
   fi  
 }
 
 downloadAndInstallPreCommitGitHook() {
+	echo "Downloading and installing .git/hooks/pre-commit file"
 	curl -s https://raw.githubusercontent.com/suhovius/gitleaks-tools/main/githooks/pre-commit > .git/hooks/pre-commit
+	echo "Making .git/hooks/pre-commit file executable"
 	chmod +x .git/hooks/pre-commit
 }
 
-checkIfPreCommitExists
 ensureGitleaksInstalled
+checkIfPreCommitExists
