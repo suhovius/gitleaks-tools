35b08e5855c0e768d943ae2b3e6b4fbe3d38deb1
Suhovius <alexey.suhoviy@gmail.com>
2024-08-26 21:15:08 +0300

    Add detect check. Refactor code

--- githooks-install.sh
+++ githooks-install.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-checkIfPreCommitExists() {
-	if [ -f .git/hooks/pre-commit ]; then
-		echo "WARNING! .git/hooks/pre-commit file already exists!"
-		read -p "Override? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
-		echo "Overriding the existing .git/hooks/pre-commit file"
-		downloadAndInstallPreCommitGitHook
-	else
-		echo "Installing new .git/hooks/pre-commit file"
-		downloadAndInstallPreCommitGitHook
-	fi
-}
-
 ensureGitleaksInstalled() {
   # Check if gitleaks is installed
   if ! [ -x "$(command -v gitleaks)" ]; then
@@ -27,6 +15,18 @@ ensureGitleaksInstalled() {
   fi  
 }
 
+checkIfPreCommitExists() {
+	if [ -f .git/hooks/pre-commit ]; then
+		echo "WARNING! .git/hooks/pre-commit file already exists!"
+		read -p "Override? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+		echo "Overriding the existing .git/hooks/pre-commit file"
+		downloadAndInstallPreCommitGitHook
+	else
+		echo "Installing new .git/hooks/pre-commit file"
+		downloadAndInstallPreCommitGitHook
+	fi
+}
+
 downloadAndInstallPreCommitGitHook() {
 	echo "Downloading and installing .git/hooks/pre-commit file"
 	curl -s https://raw.githubusercontent.com/suhovius/gitleaks-tools/main/githooks/pre-commit > .git/hooks/pre-commit
@@ -34,7 +34,31 @@ downloadAndInstallPreCommitGitHook() {
 	chmod +x .git/hooks/pre-commit
 }
 
+printPostInstallMessage() {
+	printf "GitLeaks pre-commit git hook configuration options:\n"
+	printf "\n"
+	printf "To enable protect check on the staged changes, use command:\n"
+	printf "\tgit config hooks.gitleaks.protect true\n"
+	printf "\n"
+
+	printf "To enable detect check on the commited changes, use command:\n"
+	printf "\tgit config hooks.gitleaks.detect true\n"
+	printf "\n"
+
+	printf "To disable gitleaks pre-commit hooks use such commands:"
+	printf "\n"
+	printf "\tgit config hooks.gitleaks.protect false\n"
+	printf "\tgit config hooks.gitleaks.detect false\n"
+	printf "\n"
+
+	printf "Or unset this config parameter completely if you do not need it anymore:\n"
+	printf "\tgit config --unset-all --bool hooks.gitleaks.protect\n"
+	printf "\tgit config --unset-all --bool hooks.gitleaks.detect\n"
+}
+
 ensureGitleaksInstalled
 printf "\n"
 checkIfPreCommitExists
+printf "\n"
+printPostInstallMessage
 printf "\n"
